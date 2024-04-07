3f523f5d1167fdfaa6cc7b9a0d9b70f7984c51ff
hutanmihai <hutanmihai29@gmail.com>
2024-04-07 19:57:02 +0300

    smth...

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,9 +2,14 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
-  paths: {
-    artifacts: "./client/src/artifacts",
+  solidity: {
+    version: "0.8.24",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
   },
 };
 
